9ee38ceecbcbdd7caa9bf2e1fda6fb2d1ec71578
Tianyi Lin <lint25@cs.washington.edu>
2016-12-08 16:16:56 -0800

    create basic modal for examples

--- src/components/header/renderer.js
+++ src/components/header/renderer.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Menu, {SubMenu, MenuItem, ItemGroup} from 'rc-menu';
+import ReactDOM from 'react-dom';
+import Portal from 'react-portal';
+//import Menu, {SubMenu, MenuItem, ItemGroup} from 'rc-menu';
 import { SPECS } from '../../constants';
 import 'rc-menu/assets/index.css';
 import './index.css';
@@ -15,6 +17,14 @@ export default class Header extends React.Component {
   // static propTypes = {
   //   updateVegaSpec: React.PropTypes.function
   // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPortalOpened: false,
+      someValue: 'init',
+    };
+    this.onSelectVega = this.onSelectVega.bind(this);
+  }
 
   onSelectVega (name) {
     const spec = require(`../../../spec/vega/${name}.vg.json`);
@@ -26,8 +36,8 @@ export default class Header extends React.Component {
     this.props.updateVegaLiteSpec(JSON.stringify(spec, null, 2));
   }
 
-  onSelect (selection) {
-    const key = selection.key;
+  onSelect (e) {
+    var key = e.target.value;
     if (key.startsWith('vega-lite-')) {
       this.onSelectVegaLite(key.substr(10));
     } else if (key.startsWith('vega-')) {
@@ -37,55 +47,133 @@ export default class Header extends React.Component {
     } else if (key === 'custom-vega-lite') {
       this.props.updateVegaLiteSpec('{}');
     }
+    this.setState({
+      isOpened: false
+    });
   }
 
   render () {
+    const button = (
+      <button className='exampleButton'
+        onClick={(e) => {
+          const bodyRect = document.body.getBoundingClientRect();
+          this.setState({
+            isOpened: true
+          });
+        }}
+      >
+        {'Examples'}
+      </button>
+    );
+
     return (
-      <div className='header'>
+      <div className='header'> 
         <img height={57} alt="IDL Logo" src="https://vega.github.io/images/idl-logo.png" />
-        <Menu onSelect={this.onSelect.bind(this)} mode="horizontal" defaultActiveFirst={true}>
-          <SubMenu title="Vega Examples">
-            {
-              Object.keys(SPECS.Vega).map((specType) => {
-                const specs = SPECS.Vega[specType];
-                return (
-                  <ItemGroup key={`vega-${specType}`} title={specType}>
-                    {
-                      specs.map((spec) => {
-                        return (
-                          <MenuItem key={`vega-${spec.name}`}>{formatExampleName(spec.name)}</MenuItem>
-                        )
-                      })
-                    }
-                  </ItemGroup>
-                );
-              })
-            }
-          </SubMenu>
-          <SubMenu title="Vega-Lite Examples">
-            {
-              Object.keys(SPECS.VegaLite).map((specType) => {
-                const specs = SPECS.VegaLite[specType];
-                return (
-                  <ItemGroup key={`vega-${specType}`} title={specType}>
-                  {
-                    specs.map((spec) => {
+        
+          <div>
+          {button}
+          <Portal
+            closeOnOutsideClick
+            isOpened={this.state.isOpened}
+            onClose={() => { this.setState({ isOpened: false });}}
+          >
+            <div className='modal'>
+              <div className="vega">
+                <p>Vega Examples</p>
+                {
+                    Object.keys(SPECS.Vega).map((specType) => {
+                      const specs = SPECS.Vega[specType];
                       return (
-                        <MenuItem key={`vega-lite-${spec.name}`}>{spec.title}</MenuItem>
-                      )
+                        <div className='itemGroup' value={`vega-${specType}`}>{specType}
+                          {
+                            specs.map((spec) => {
+                              return (
+                                <button value={`vega-${spec.name}`} onClick={this.onSelect.bind(this)}>{formatExampleName(spec.name)}</button>
+                              )
+                            })
+                          }
+                        </div>
+                      );
+                    })
+                }
+              </div>
+
+              <div className="vega-Lite" >
+                  <p>Vega-Lite Examples</p>
+                  {
+                    Object.keys(SPECS.VegaLite).map((specType) => {
+                      const specs = SPECS.VegaLite[specType];
+                      return (
+                        <div className='itemGroup' value={`vega-${specType}`}>{specType}
+                          {
+                            specs.map((spec) => {
+                              return (
+                                <button value={`vega-lite-${spec.name}`} onClick={this.onSelect.bind(this)}>{spec.title}</button>
+                              )
+                            })
+                          }
+                        </div>
+                      );
                     })
                   }
-                  </ItemGroup>
-                )
-              })
-            }
-          </SubMenu>
-          <SubMenu title="Custom">
-            <MenuItem key={`custom-vega`}>{'Vega'}</MenuItem>
-            <MenuItem key={`custom-vega-lite`}>{'Vega-Lite'}</MenuItem>
-          </SubMenu>
-        </Menu>
+              </div>
+          </div>
+        </Portal>
+        </div>
+        
       </div>
     );
   };
 };
+
+
+
+// render () {
+//     return (
+//       <div className='header'> 
+//         <img height={57} alt="IDL Logo" src="https://vega.github.io/images/idl-logo.png" />
+//         <Menu onSelect={this.onSelect.bind(this)} mode="horizontal" defaultActiveFirst={true}>
+//           <SubMenu title="Vega Examples">
+//             {
+//               Object.keys(SPECS.Vega).map((specType) => {
+//                 const specs = SPECS.Vega[specType];
+//                 return (
+//                   <ItemGroup key={`vega-${specType}`} title={specType}>
+//                     {
+//                       specs.map((spec) => {
+//                         return (
+//                           <MenuItem key={`vega-${spec.name}`}>{formatExampleName(spec.name)}</MenuItem>
+//                         )
+//                       })
+//                     }
+//                   </ItemGroup>
+//                 );
+//               })
+//             }
+//           </SubMenu>
+//           <SubMenu title="Vega-Lite Examples" >
+//             {
+//               Object.keys(SPECS.VegaLite).map((specType) => {
+//                 const specs = SPECS.VegaLite[specType];
+//                 return (
+//                   <ItemGroup key={`vega-${specType}`} title={specType}>
+//                   {
+//                     specs.map((spec) => {
+//                       return (
+//                         <MenuItem key={`vega-lite-${spec.name}`}>{spec.title}</MenuItem>
+//                       )
+//                     })
+//                   }
+//                   </ItemGroup>
+//                 )
+//               })
+//             }
+//           </SubMenu>
+//           <SubMenu title="Custom">
+//             <MenuItem key={`custom-vega`}>{'Vega'}</MenuItem>
+//             <MenuItem key={`custom-vega-lite`}>{'Vega-Lite'}</MenuItem>
+//           </SubMenu>
+//         </Menu>
+//       </div>
+//     );
+//   };
