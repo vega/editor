6b2df7b09b5452ef15377facedcae90377d2bc6c
Tianyi Lin <lint25@cs.washington.edu>
2017-04-05 13:49:11 -0700

    add local storage

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -3,18 +3,27 @@ import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
+import persistState from 'redux-localstorage';
+
+
 export default function configureStore (browserHistory, initialState = {}) {
   // Compose final middleware and use devtools in debug environment
   let middleware = applyMiddleware(thunk);
   middleware = compose(middleware, applyMiddleware(routerMiddleware(browserHistory)));
+
 
   if (process.env.NODE_ENV !== 'production') {
     const devTools = require('../components/debug/dev-tools').default.instrument();
     middleware = compose(middleware, devTools);
   }
 
+  const enhancer = compose(
+    middleware,
+    persistState(null, 'redux'),
+  )
+
   // Create final store and subscribe router in debug env ie. for devtools
-  const store = middleware(createStore)(rootReducer, initialState);
+  const store = createStore(rootReducer, initialState, enhancer);
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
