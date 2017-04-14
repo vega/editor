59e2fca036891f54b2ee3e828c70252d86cfedd9
Matthew Conlen <mc@mathisonian.com>
2017-04-13 19:15:22 -0700

    data and specs paths, etc.

--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -2,11 +2,13 @@
 TARGET=public/vendor
 DATA=public/data
 SPEC=spec
+SCHEMA=schema
 
 # Copy dependencies by default. Link if a -l flag is specified.
 CWD=$(pwd)
 VEGA_OP="cp -R"
 VEGA_DATASETS_OP="cp -R"
+SCHEMA_OP="cp"
 VEGA_EMBED_OP="cp -R"
 VEGA_LITE_OP="cp -R"
 
@@ -41,6 +43,8 @@ if [ ! -d "$DATA" ]; then
 fi
 
 eval $VEGA_DATASETS_OP "$CWD/node_modules/vega-datasets/data/*" $DATA
+# eval $VEGA_DATASETS_OP "$CWD/node_modules/vega/test/data/*" $DATA
+eval $VEGA_DATASETS_OP "$CWD/data/*" $DATA
 
 echo "Copy examples to '$SPEC'."
 
@@ -48,6 +52,9 @@ if [ ! -d "$SPEC" ]; then
   mkdir $SPEC
 fi
 
-eval $VEGA_OP "$CWD/node_modules/vega/spec" "$SPEC/vega"
+eval SCHEMA_OP "$CWD/node_modules/vega/build/vega-schema.json" "$SCHEMA/vega.schema.json" 
+eval SCHEMA_OP "$CWD/node_modules/vega-lite/build/vega-lite-schema.json" "$SCHEMA/vl.schema.json"
+
+# eval $VEGA_OP "$CWD/node_modules/vega/test/specs-valid/spec" "$SPEC/vega"
 eval $VEGA_LITE_OP "$CWD/node_modules/vega-lite/examples/specs" "$SPEC/vega-lite"
 echo "var VL_SPECS = "`cat $CWD/node_modules/vega-lite/examples/vl-examples.json` > public/js/vl-specs.js
