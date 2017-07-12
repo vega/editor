e1e0fac24b1be91be70ce17bd20e577ffa4d0aef
Dominik Moritz <domoritz@gmail.com>
2017-07-11 22:42:21 -0700

    Fix vendor script for vega-lite

--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -58,5 +58,5 @@ eval $SCHEMA_OP "$CWD/node_modules/vega-lite/build/vega-lite-schema.json" "$SCHE
 eval $VEGA_OP "$CWD/node_modules/vega/docs/examples/*.vg.json" "$SPEC/vega"
 eval $VEGA_LITE_OP "$CWD/node_modules/vega-lite/examples/specs/" "$SPEC/vega-lite/"
 
-cp "$CWD/node_modules/vega-lite/examples/vl-examples.json" "$SPEC/vega-lite/index.json"
+cp "$CWD/node_modules/vega-lite/_data/examples.json" "$SPEC/vega-lite/index.json"
 cp "$CWD/node_modules/vega/docs/_data/examples.json" "$SPEC/vega/index.json"
