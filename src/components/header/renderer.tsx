13e9704cc7559296b66a1fa5c5484e37943f7df6
Yash Dev Lamba <yashdev18@gmail.com>
2018-02-20 04:37:31 +0530

    Closing modal after selection added

--- src/components/header/renderer.tsx
+++ src/components/header/renderer.tsx
@@ -108,76 +108,121 @@ class Header extends React.Component<Props & {history: any}, State> {
         {'New'}
       </div>
     );
-    const vega = (
-      <div className='vega'>
-        {Object.keys(VEGA_SPECS).map((specType, i) => {
-          const specs = VEGA_SPECS[specType];
-          return (
-            <div className='itemGroup' key={i}>
-              <div className='specType'>{specType}</div>
-              <div className='items'>
-                {specs.map((spec, j) => {
-                  return (
-                    <div key={j} onClick={() => this.onSelectVega(spec.name)} className='item'>
-                      <div style={{backgroundImage: `url(images/examples/vg/${spec.name}.vg.png)`}} className='img'/>
-                      <div className='name'>{formatExampleName(spec.name)}</div>
-                    </div>
-                  );
-                })}
+    const vega = (closePortal) => {
+      return (
+        <div className='vega'>
+          {Object.keys(VEGA_SPECS).map((specType, i) => {
+            const specs = VEGA_SPECS[specType];
+            return (
+              <div className='itemGroup' key={i}>
+                <div className='specType'>{specType}</div>
+                <div className='items'>
+                  {specs.map((spec, j) => {
+                    return (
+                      <div
+                        key={j}
+                        onClick={() => {
+                          this.onSelectVega(spec.name);
+                          closePortal();
+                        }}
+                        className='item'
+                      >
+                        <div
+                          style={{
+                            backgroundImage: `url(images/examples/vg/${
+                              spec.name
+                            }.vg.png)`,
+                          }}
+                          className='img'
+                        />
+                        <div className='name'>{formatExampleName(spec.name)}</div>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
-    );
-    const vegalite = (
-      <div className='vega-Lite'>
-        {Object.keys(VEGA_LITE_SPECS).map((specType, i) => {
-          const specs = VEGA_LITE_SPECS[specType];
-          return (
-            <div className='itemGroup' key={i}>
-              <div className='specType'>{specType}</div>
-              <div className='items'>
-                {specs.map((spec, j) => {
-                  return (
-                    <div key={j} onClick={() => this.onSelectVegaLite(spec.name)} className='item'>
-                      <div style={{backgroundImage: `url(images/examples/vl/${spec.name}.vl.png)`}} className='img'/>
-                      <div className='name'>{spec.title}</div>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    );
-    const gist = (
-      <div>
-        <header>Enter Gist URL: </header>
-        <div className='gist-content'>
-          <div className='gist-text'>For example (Vega-Lite)</div>
-          <div className='gist-url'>
-            https://gist.github.com/domoritz/455e1c7872c4b38a58b90df0c3d7b1b9
-          </div>
-
-          <input
-            className='gist-input'
-            type='text'
-            placeholder='enter gist url here'
-            value={this.state.url}
-            onChange={this.handleChange.bind(this)}
-          />
-
-          <button className='gist-button' onClick={this.onSelectVegaGist.bind(this, this.state.url)}>
-            Vega
-          </button>
-          <button className='gist-button' onClick={this.onSelectVegaLiteGist.bind(this, this.state.url)}>
-            Vega-Lite
-          </button>
+            );
+          })}
         </div>
-      </div>
-    );
+      );
+    }
+    const vegalite = (closePortal) => {
+      return (
+        <div className='vega-Lite'>
+          {Object.keys(VEGA_LITE_SPECS).map((specType, i) => {
+            const specs = VEGA_LITE_SPECS[specType];
+            return (
+              <div className='itemGroup' key={i}>
+                <div className='specType'>{specType}</div>
+                <div className='items'>
+                  {specs.map((spec, j) => {
+                    return (
+                      <div
+                        key={j}
+                        onClick={() => {
+                          this.onSelectVegaLite(spec.name);
+                          closePortal();
+                        }}
+                        className='item'
+                      >
+                        <div
+                          style={{
+                            backgroundImage: `url(images/examples/vl/${
+                              spec.name
+                            }.vl.png)`,
+                          }}
+                          className='img'
+                        />
+                        <div className='name'>{spec.title}</div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+    const gist = (closePortal) => {
+      return (
+        <div>
+          <header>Enter Gist URL: </header>
+          <div className='gist-content'>
+            <div className='gist-text'>For example (Vega-Lite)</div>
+            <div className='gist-url'>
+            https://gist.github.com/domoritz/455e1c7872c4b38a58b90df0c3d7b1b9
+            </div>
+            <input
+              className='gist-input'
+              type='text'
+              placeholder='enter gist url here'
+              value={this.state.url}
+              onChange={this.handleChange.bind(this)}
+            />
+
+            <button
+              className='gist-button'
+              onClick={() => {
+                this.onSelectVegaGist(this.state.url);
+                closePortal();
+              }}
+            >
+              Vega
+            </button>
+            <button
+              className='gist-button'
+              onClick={() => {
+                this.onSelectVegaLiteGist(this.state.url);
+                closePortal();
+              }}
+            >
+              Vega-Lite
+            </button>
+          </div>
+        </div>
+      );
+    }
     return (
       <div className='header'>
         <a
@@ -226,15 +271,15 @@ class Header extends React.Component<Props & {history: any}, State> {
         <PortalWithState closeOnEsc>
           {({openPortal, closePortal, isOpen, portal}) => [
             <span
-              key="0"
+              key='0'
               onClick={openPortal}
             >
               {examplesButton}
             </span>,
             portal(
-              <div className="modal-background">
-                <div className="modal-header">
-                  <div className="button-groups">
+              <div className='modal-background' onClick={closePortal}>
+                <div className='modal-header'>
+                  <div className='button-groups' onClick={(e) => {e.stopPropagation()}}>
                     <button
                       className={this.state.showVega ? 'selected' : ''}
                       onClick={() => {
@@ -254,17 +299,15 @@ class Header extends React.Component<Props & {history: any}, State> {
                   </div>
 
                   <button
-                    className="close-button"
-                    onClick={() => {
-                      closePortal
-                    }}
+                    className='close-button'
+                    onClick={closePortal}
                   >
                     ✖
                   </button>
                 </div>
-                <div className="modal-area">
-                  <div className="modal">
-                    {this.state.showVega ? vega : vegalite}
+                <div className='modal-area'>
+                  <div className='modal' onClick={(e) => {e.stopPropagation()}}>
+                    {this.state.showVega ? vega(closePortal) : vegalite(closePortal)}
                   </div>
                 </div>
               </div>
@@ -275,25 +318,23 @@ class Header extends React.Component<Props & {history: any}, State> {
         <PortalWithState closeOnEsc>
           {({openPortal, closePortal, isOpen, portal}) => [
             <span
-              key="0"
+              key='0'
               onClick={openPortal}
             >
               {gistButton}
             </span>,
             portal(
-              <div className="modal-background">
-                <div className="modal-header">
+              <div className='modal-background' onClick={closePortal}>
+                <div className='modal-header'>
                   <button
-                    className="close-button"
-                    onClick={() => {
-                      closePortal
-                    }}
+                    className='close-button'
+                    onClick={closePortal}
                   >
                     ✖
                   </button>
                 </div>
-                <div className="modal-area">
-                  <div className="modal">{gist}</div>
+                <div className='modal-area'>
+                  <div className='modal' onClick={(e) => {e.stopPropagation()}}>{gist(closePortal)}</div>
                 </div>
               </div>
             )
@@ -302,7 +343,7 @@ class Header extends React.Component<Props & {history: any}, State> {
 
         <span>{docsLink}</span>
         <span>{customButton}</span>
-        </div>
+      </div>
     );
   }
 }
