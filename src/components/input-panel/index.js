a18ec7a2cdb2dcc4f3e1b5ff2c4d2bdfe3241f54
Dominik Moritz <domoritz@gmail.com>
2018-02-19 13:22:57 -0800

    Prevent new editor instance when opening Vega-Lite editor. Fixes #21, #12. Thanks to @saurabh0402

--- src/components/input-panel/index.js
+++ src/components/input-panel/index.js
@@ -31,10 +31,17 @@ class InputPanel extends React.Component {
   render() {
     const innerPanes = this.getInnerPanes();
 
-    if (this.props.mode === MODES.VegaLite && this.props.compiledVegaSpec) {
+    if (this.props.mode === MODES.VegaLite) {
+      if (this.props.compiledVegaSpec) {
         return <SplitPane split="horizontal" defaultSize={(window.innerHeight - LAYOUT.HeaderHeight) / innerPanes.length} pane2Style={{display: 'flex'}}>
           {innerPanes}
         </SplitPane>;
+      } else {
+        // Use the same split pane as above to prevent the creation of a new monaco instance.
+        return <SplitPane split="horizontal" primary="second" defaultSize={25} pane1Style={{display: 'flex'}}>
+          {innerPanes}
+        </SplitPane>;
+      }
     } else {
       return <div className={'full-height-wrapper'}>
         {innerPanes}
