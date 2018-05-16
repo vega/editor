bb8b7ba8a3661f642ffbfdb29bfaee3c60fef5d1
Yash Dev Lamba <yashdev18@gmail.com>
2018-05-16 20:49:35 +0530

    Minor updates in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,43 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = env => {
   const config = {
     entry: {
-        main: "./src/index.tsx"
+        main: './src/index.tsx'
     },
     output: {
-        filename: "static/js/[name].bundle.js",
+        filename: 'static/js/[name].bundle.js',
         chunkFilename: 'static/js/[name].chunk.js',
-        path: path.resolve(__dirname, "build"),
+        path: path.resolve(__dirname, 'build'),
     },
 
     // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+    devtool: 'source-map',
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: ['.ts', '.tsx', '.js', '.json']
     },
 
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
             { 
               test: /\.tsx?$/,
-              loader: "awesome-typescript-loader",
+              use: ['ts-loader'],
             },
             {
               test: /\.css$/,
-              use: [ 'style-loader', 'css-loader' ]
-            },
-            {
-              test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-              loader: 'url-loader',
-              options: {
-                limit: 10000
-              }
+              use: ['style-loader', 'css-loader']
             }
         ]
     },
     plugins: [
       // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin({
+        filename: 'index.html',
         template: 'public/index.html',
         favicon: 'public/favicon.ico'
       }),
