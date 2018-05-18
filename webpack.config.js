46508ec0c4144193b0a21e3e14055fd7733a1450
Dominik Moritz <domoritz@gmail.com>
2018-05-17 19:41:55 -0700

    Remove babel, format code

--- webpack.config.js
+++ webpack.config.js
@@ -6,35 +6,35 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 module.exports = env => {
   const config = {
     entry: {
-        main: './src/index.tsx'
+      main: './src/index.tsx'
     },
     output: {
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].chunk.js',
-        path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js',
+      chunkFilename: '[name].chunk.js',
+      path: path.resolve(__dirname, 'dist')
     },
 
     // Enable sourcemaps for debugging webpack's output.
     devtool: 'source-map',
 
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ['.ts', '.tsx', '.js', '.json']
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: ['.ts', '.tsx', '.js', '.json']
     },
 
     module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-            { 
-              test: /\.tsx?$/,
-              use: ['ts-loader'],
-            },
-             // All CSS files will be handled by 'css-loader' & `styles-loader`.
-            {
-              test: /\.css$/,
-              use: ['style-loader', 'css-loader']
-            }
-        ]
+      rules: [
+        // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+        {
+          test: /\.tsx?$/,
+          use: ['ts-loader']
+        },
+        // All CSS files will be handled by 'css-loader' & `styles-loader`.
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
     },
     plugins: [
       // Generates an `index.html` file with the <script> injected.
@@ -51,7 +51,7 @@ module.exports = env => {
     ],
     // Configurations for `webpack-dev-server`
     devServer: {
-      contentBase: path.join(__dirname, "public"),
+      contentBase: path.join(__dirname, 'public')
     },
     node: {
       fs: 'empty'
