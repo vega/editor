433c87b1ad7ee4da07a8640fcd2a9a3f2c7862c8
Yash Dev Lamba <yashdev18@gmail.com>
2018-07-18 20:51:44 +0530

    Update debug pane

--- src/components/viz-pane/renderer.tsx
+++ src/components/viz-pane/renderer.tsx
@@ -1,7 +1,6 @@
 import './index.css';
 
 import * as React from 'react';
-import { ChevronDown, ChevronUp, X } from 'react-feather';
 import SplitPane from 'react-split-pane';
 
 import DataViewer from '../data-viewer';
@@ -17,17 +16,21 @@ interface Props {
 }
 
 interface State {
-  errorLogs: boolean;
+  logs: boolean;
 }
 
 export default class VizPane extends React.Component<Props, State> {
   constructor(props) {
     super(props);
     this.state = {
-      errorLogs: true,
+      logs: true,
     };
   }
   public render() {
+    const debugPane = this.refs.debugPane as any;
+    if (debugPane) {
+      debugPane.pane2.style.height = this.props.debugPane ? window.innerHeight * 0.4 + 'px' : '25px';
+    }
     const container = (
       <div className="chart-container">
         <ErrorBoundary>
@@ -40,65 +43,45 @@ export default class VizPane extends React.Component<Props, State> {
       <div className="debug-pane-header" onClick={e => this.props.toggleDebugPane()}>
         <ul className="tabs-nav">
           <li
-            className={this.state.errorLogs ? 'active-tab' : ''}
+            className={this.state.logs ? 'active-tab' : ''}
             onClick={e => {
               if (this.props.debugPane) {
                 e.stopPropagation();
               }
-              this.setState({ errorLogs: true });
+              this.setState({ logs: true });
             }}
           >
-            Error Logs
+            Logs
           </li>
           <li
-            className={this.state.errorLogs ? '' : 'active-tab'}
+            className={this.state.logs ? '' : 'active-tab'}
             onClick={e => {
               if (this.props.debugPane) {
                 e.stopPropagation();
               }
-              this.setState({ errorLogs: false });
+              this.setState({ logs: false });
             }}
           >
             Data Viewer
           </li>
         </ul>
-        <span onClick={e => this.props.toggleDebugPane()} className="close">
-          <X size={20} />
-        </span>
       </div>
     );
-    if (this.props.debugPane) {
-      return (
-        <SplitPane
-          split="horizontal"
-          primary="second"
-          minSize={25}
-          defaultSize={window.innerHeight * 0.4}
-          paneStyle={{ display: 'flex' }}
-        >
-          {container}
-          <div className="debug-pane">
-            {debugPaneHeader}
-            {this.state.errorLogs ? <ErrorPane /> : <DataViewer />}
-          </div>
-        </SplitPane>
-      );
-    } else {
-      return (
-        <SplitPane
-          split="horizontal"
-          primary="second"
-          minSize={25}
-          defaultSize={25}
-          paneStyle={{ display: 'flex', height: '25px' }}
-        >
-          {container}
-          <div className="debug-pane">
-            {debugPaneHeader}
-            {this.state.errorLogs ? <ErrorPane /> : <DataViewer />}
-          </div>
-        </SplitPane>
-      );
-    }
+    return (
+      <SplitPane
+        ref="debugPane"
+        split="horizontal"
+        primary="second"
+        minSize={25}
+        defaultSize={25}
+        paneStyle={{ display: 'flex' }}
+      >
+        {container}
+        <div className="debug-pane">
+          {debugPaneHeader}
+          {this.state.logs ? <ErrorPane /> : <DataViewer />}
+        </div>
+      </SplitPane>
+    );
   }
 }
