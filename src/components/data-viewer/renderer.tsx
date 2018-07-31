ae7aa585c1556a7198b6cd76f7a0acfee263b7cf
Yash Dev Lamba <yashdev18@gmail.com>
2018-08-01 04:07:20 +0530

    Implement pagination (100 per page)

--- src/components/data-viewer/renderer.tsx
+++ src/components/data-viewer/renderer.tsx
@@ -1,6 +1,7 @@
 import './index.css';
 
 import * as React from 'react';
+import ReactPaginate from 'react-paginate';
 import Select from 'react-select';
 import * as vega from 'vega';
 import { stringify } from 'vega-tooltip';
@@ -12,24 +13,34 @@ interface Props {
 }
 
 interface State {
+  currentPage: number;
+  pageCount: number;
   selectedData: string;
 }
 
 const OPTIONS = {
   maxDepth: 2,
+  perPage: 100,
 };
 
 export default class ErrorPane extends React.Component<Props, State> {
   constructor(props) {
     super(props);
     this.state = {
+      currentPage: 0,
+      pageCount: 1,
       selectedData: 'root',
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handlePageChange = this.handlePageChange.bind(this);
     this.formatData = this.formatData.bind(this);
   }
   public handleChange(option) {
-    this.setState({ selectedData: option.value });
+    this.setState({ selectedData: option.value, currentPage: 0 });
+  }
+  public handlePageChange(option) {
+    const selected = option.selected;
+    this.setState({ currentPage: selected });
   }
   public getDataList() {
     return Object.keys(this.props.view.getState({ data: vega.truthy, signals: vega.falsy, recurse: true }).data);
@@ -58,17 +69,23 @@ export default class ErrorPane extends React.Component<Props, State> {
       return;
     }
     let table = '<table><tr>';
+    // Creating table header using first object
     Object.keys(data[0]).map(key => {
       table += `<th>${key}</th>`;
     });
     table += '</tr>';
-    Object.values(data).map(key => {
+    // Content of table
+    const content = Object.values(data);
+    const start = OPTIONS.perPage * this.state.currentPage;
+    const end = start + OPTIONS.perPage;
+    content.slice(start, end).map(key => {
       table += '<tr>';
       Object.values(key).map(value => {
+        // Replacing large data with (...)
         // Width of table cell / average width of a char i.e 8px
         if (value && value.length > 24) {
           value = this.escapeHTML(value);
-          table += `<td data-tip="${value}">(...)</td>`;
+          table += `<td>(...)</td>`;
         } else {
           table += `<td>${value}</td>`;
         }
@@ -76,6 +93,7 @@ export default class ErrorPane extends React.Component<Props, State> {
       table += '</tr>';
     });
     table += '</table>';
+
     return table;
   }
   public escapeHTML(data: string) {
@@ -89,6 +107,7 @@ export default class ErrorPane extends React.Component<Props, State> {
   public render() {
     let table;
     let select;
+    let pageCount;
     const options = [];
     const dataList = this.getDataList();
     if (dataList) {
@@ -111,6 +130,7 @@ export default class ErrorPane extends React.Component<Props, State> {
       if (dataList.includes(this.state.selectedData)) {
         const data = this.getData(this.state.selectedData);
         if (data) {
+          pageCount = Math.ceil(data.length / OPTIONS.perPage);
           table = this.generateTable(this.formatData(data));
         }
       } else {
@@ -119,7 +139,19 @@ export default class ErrorPane extends React.Component<Props, State> {
     }
     return (
       <div className="data-viewer">
-        {select ? select : ''}
+        <div className="data-viewer-header">
+          {select ? select : ''}
+          <ReactPaginate
+            previousLabel={'<'}
+            nextLabel={'>'}
+            pageCount={pageCount}
+            marginPagesDisplayed={1}
+            pageRangeDisplayed={3}
+            onPageChange={this.handlePageChange}
+            containerClassName={'pagination'}
+            activeClassName={'active'}
+          />
+        </div>
         <div className="data-table" dangerouslySetInnerHTML={{ __html: table }} />
       </div>
     );
