fd1af616def689c2ad7182e3df964631a8cf4a98
Dominik Moritz <domoritz@gmail.com>
2019-01-21 01:48:01 -0800

    Update to Vega-Lite v3. Fixes #198

--- src/constants/consts.ts
+++ src/constants/consts.ts
@@ -29,7 +29,7 @@ export const VEGA_START_SPEC = `{
 }`;
 
 export const VEGA_LITE_START_SPEC = `{
-  "$schema": "https://vega.github.io/schema/vega-lite/v2.json"
+  "$schema": "https://vega.github.io/schema/vega-lite/v3.json"
 }`;
 
 export type View = vega.View;
