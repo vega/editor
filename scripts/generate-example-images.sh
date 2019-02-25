f9a19d2d69c97e2b3987493fdb72987a1d96645a
Dominik Moritz <domoritz@gmail.com>
2019-02-24 20:19:22 -0800

    Rebuild images

--- scripts/generate-example-images.sh
+++ scripts/generate-example-images.sh
@@ -6,8 +6,11 @@ set -e
 mkdir -p public/images/examples/vl
 mkdir -p public/images/examples/vg
 
-echo "Generating SVGs..."
 cd public;
 
-ls spec/vega-lite/*.vl.json | parallel --eta --halt 1 "../node_modules/.bin/vl2png {} images/examples/vl/{/.}.png"
-ls spec/vega/*.vg.json | parallel --eta --halt 1 "../node_modules/.bin/vg2png {} images/examples/vg/{/.}.png"
+echo "Generating PNGs for Vega-Lite..."
+ls spec/vega-lite/*.vl.json | parallel --eta --halt 1 "../node_modules/.bin/vl2vg {} | ../node_modules/.bin/vg2png > images/examples/vl/{/.}.png"
+
+echo "Generating PNGs for Vega..."
+ls spec/vega/*.vg.json | parallel --eta --halt 1 "../node_modules/.bin/vg2png -b . {} > images/examples/vg/{/.}.png"
+
