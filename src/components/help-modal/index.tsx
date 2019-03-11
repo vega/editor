8634c6fc8635aaf330236d007df7b0501024aa3b
siddhant1 <sidhant.manchanda@gmail.com>
2019-03-11 22:55:55 +0530

    Added Tips

--- src/components/help-modal/index.tsx
+++ src/components/help-modal/index.tsx
@@ -21,24 +21,32 @@ const HelpModal = () => {
           <kbd>Alt</kbd> + <kbd> Shift </kbd> + <kbd> f </kbd> / <kbd>&#8997;</kbd> + <kbd>&#8984;</kbd> + <kbd>f</kbd>:
           Auto format
         </li>
+        <li>
+          <kbd>Ctrl</kbd> + <kbd> f11 </kbd> / <kbd>&#8984;</kbd> + <kbd>f11</kbd>: Toggle Fullscreen Mode
+        </li>
       </ul>
       <a href="https://github.com/vega/editor/issues/new" target="_blank" className="report-button">
         <AlertCircle className="header-icon" />
         Report a Bug{' '}
       </a>
-      <br />
       <h2>Have a feedback? Let us know</h2>
-      <a href="https://bit.ly/join-vega-slack" target="_blank">
+      <a href="https://bit.ly/join-vega-slack" target="_blank" className="link-to-docs">
+        <img src="/images/utils/slack-logo-icon.png" className="doc-image" />
         Join our slack channel
       </a>
-      <br />
+      <a href="https://twitter.com/vega_vis" target="_blank" className="link-to-docs">
+        <img src="/images/utils/Twitter_Bird.svg.png" className="doc-image" />
+        Follow us on twitter
+      </a>
       <h2>References</h2>
-      <a href="https://vega.github.io/vega-lite/" target="_blank">
+      <a href="https://vega.github.io/vega-lite/" target="_blank" className="link-to-docs">
         Vega-lite Docs
       </a>
-      <a href="https://vega.github.io/vega/" target="_blank">
+      <a href="https://vega.github.io/vega/" target="_blank" className="link-to-docs">
         Vega Docs
       </a>
+      <h2>Tips</h2>
+      1. You can access and debug vega view using your browsers developer console
     </div>
   );
 };
