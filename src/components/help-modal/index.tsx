0aab7836f42049aa745612d4dce55061cc03e28e
siddhant1 <sidhant.manchanda@gmail.com>
2019-03-15 18:32:35 +0530

    Added OS Specific Shortcuts

--- src/components/help-modal/index.tsx
+++ src/components/help-modal/index.tsx
@@ -2,8 +2,22 @@ import React from 'react';
 import { AlertCircle, Slack, Twitter } from 'react-feather';
 import { version } from 'vega';
 import { version as VegaLiteVersion } from 'vega-lite';
+import isMac from '../../utils/isMac';
+import shortcuts from '../../utils/keyboardShortcuts';
 import './index.css';
 const pjson = require('../../../package.json');
+
+const keyBoardShortcuts = shortcuts.map(shortcut => {
+  return isMac() ? (
+    <li>
+      {shortcut.mac.split(' ').map(key => (key === '+' ? '+' : <kbd>{key}</kbd>))} : {shortcut.text}
+    </li>
+  ) : (
+    <li>
+      {shortcut.windows.split(' ').map(key => (key === '+' ? '+' : <kbd>{key}</kbd>))} : {shortcut.text}
+    </li>
+  );
+});
 
 const HelpModal = () => {
   return (
@@ -11,24 +25,7 @@ const HelpModal = () => {
       <div className="help-header">
         <h1 className="modal-header">Help</h1>
         <h2 className="modal-title">Keyboard Shortcuts</h2>
-        <ul className="keyboard-shortcuts">
-          <li>
-            <kbd>Ctrl</kbd> + <kbd>b</kbd> / <kbd>&#8984;</kbd> + <kbd>b</kbd>: Execute the code in manual mode
-          </li>
-          <li>
-            <kbd>Ctrl</kbd> + <kbd>?</kbd> / <kbd>&#8984;</kbd> + <kbd>'</kbd>: Open the help window
-          </li>
-          <li>
-            <kbd>Ctrl</kbd> + <kbd>Space</kbd> / <kbd>&#8984;</kbd> + <kbd>Space</kbd>: Open Intellisense
-          </li>
-          <li>
-            <kbd>Alt</kbd> + <kbd> Shift </kbd> + <kbd> f </kbd> / <kbd>&#8997;</kbd> + <kbd>&#8984;</kbd> +{' '}
-            <kbd>f</kbd>: Auto format
-          </li>
-          <li>
-            <kbd>Ctrl</kbd> + <kbd> f11 </kbd> / <kbd>&#8984;</kbd> + <kbd>f11</kbd>: Toggle Fullscreen Mode
-          </li>
-        </ul>
+        <ul className="keyboard-shortcuts">{keyBoardShortcuts}</ul>
         <a href="https://github.com/vega/editor/issues/new" target="_blank" className="report-button help-modal-link">
           <AlertCircle className="header-icon" />
           Report a Bug{' '}
---
+++ src/utils/keyboardShortcuts.ts
@@ -0,0 +1,29 @@
+const shortcuts = [
+  {
+    mac: '&#8984; + b',
+    text: 'Execute the code in manual mode',
+    windows: 'Ctrl + b',
+  },
+  {
+    mac: '&#8984; + ?',
+    text: 'Open the help window',
+    windows: 'Ctrl + ?',
+  },
+  {
+    mac: '&#8984; + Space',
+    text: 'Open Intellisense',
+    windows: 'Ctrl + Space',
+  },
+  {
+    mac: '&#8997; + &#8984; + b',
+    text: 'Auto format',
+    windows: 'Alt + shift + f',
+  },
+  {
+    mac: '&#8984; + f11',
+    text: 'Toggle Fullscreen Mode',
+    windows: 'Ctrl + f11',
+  },
+];
+
+export default shortcuts;
