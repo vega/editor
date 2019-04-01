e2862f1b240599209574fb85b5bf52aa2d8faa1a
ganeshpatro321 <ganeshpatro321@gmail.com>
2019-04-01 17:09:21 +0530

    Extracted component

--- src/components/header/renderer.tsx
+++ src/components/header/renderer.tsx
@@ -28,6 +28,7 @@ import { NAMES } from '../../constants/consts';
 import { VEGA_LITE_SPECS, VEGA_SPECS } from '../../constants/specs';
 import HelpModal from '../help-modal/index';
 import './index.css';
+import ShareModal from './share-modal/index';
 
 type Props = ReturnType<typeof mapStateToProps> &
   ReturnType<typeof mapDispatchToProps> & { history: any; showExample: boolean };
@@ -734,59 +735,61 @@ class Header extends React.Component<Props, State> {
       </div>
     );
 
-    const shareContent = (
-      <div className="share-content">
-        <h2>Share</h2>
-        <p>We pack the Vega or Vega-Lite specification and an encoded string in the URL.</p>
-        <p>We use LZ-based compression algorithm and preserve indentation, newlines, and other whitespace.</p>
-        <div className="user-pref">
-          <label>
-            Link opens visualization in fullscreen:
-            <input
-              type="checkbox"
-              defaultChecked={this.state.fullscreen}
-              name="fullscreen"
-              onChange={this.handleCheck.bind(this)}
-            />
-          </label>
-        </div>
-        <div className="share-buttons">
-          <button className="share-button" onClick={() => this.previewURL()}>
-            <Link />
-            <span>Open Link</span>
-          </button>
-          <Clipboard
-            className="share-button copy-icon"
-            data-clipboard-text={this.state.generatedURL}
-            onSuccess={this.onCopy.bind(this)}
-          >
-            <span>
-              <Copy />
-              Copy to Clipboard
-            </span>
-          </Clipboard>
-          <div className={`copied + ${this.state.copied ? ' visible' : ''}`}>Copied!</div>
-        </div>
-        <div className="byte-counter">
-          Number of charaters in the URL: {this.state.generatedURL.length}{' '}
-          <span className="url-warning">
-            {this.state.generatedURL.length > 2083 ? (
-              <span>
-                Warning:{' '}
-                <a
-                  href="https://support.microsoft.com/en-us/help/208427/maximum-url-length-is-2-083-characters-in-internet-explorer"
-                  target="_blank"
-                >
-                  URLs over 2083 characters may not be supported in Internet Explorer.
-                </a>
-              </span>
-            ) : (
-              ''
-            )}
-          </span>
-        </div>
-      </div>
-    );
+    // const shareContent = (
+    //   <div className="share-content">
+    //     <h2>Share</h2>
+    //     <p>We pack the Vega or Vega-Lite specification and an encoded string in the URL.</p>
+    //     <p>We use LZ-based compression algorithm and preserve indentation, newlines, and other whitespace.</p>
+    //     <div className="user-pref">
+    //       <label>
+    //         Link opens visualization in fullscreen:
+    //         <input
+    //           type="checkbox"
+    //           defaultChecked={this.state.fullscreen}
+    //           name="fullscreen"
+    //           onChange={this.handleCheck.bind(this)}
+    //         />
+    //       </label>
+    //     </div>
+    //     <div className="share-buttons">
+    //       <button className="share-button" onClick={() => this.previewURL()}>
+    //         <Link />
+    //         <span>Open Link</span>
+    //       </button>
+    //       <Clipboard
+    //         className="share-button copy-icon"
+    //         data-clipboard-text={this.state.generatedURL}
+    //         onSuccess={this.onCopy.bind(this)}
+    //       >
+    //         <span>
+    //           <Copy />
+    //           Copy to Clipboard
+    //         </span>
+    //       </Clipboard>
+    //       <div className={`copied + ${this.state.copied ? ' visible' : ''}`}>Copied!</div>
+    //     </div>
+    //     <div className="byte-counter">
+    //       Number of charaters in the URL: {this.state.generatedURL.length}{' '}
+    //       <span className="url-warning">
+    //         {this.state.generatedURL.length > 2083 ? (
+    //           <span>
+    //             Warning:{' '}
+    //             <a
+    //               href="https://support.microsoft.com/en-us/help/208427/maximum-url-length-is-2-083-characters-in-internet-explorer"
+    //               target="_blank"
+    //             >
+    //               URLs over 2083 characters may not be supported in Internet Explorer.
+    //             </a>
+    //           </span>
+    //         ) : (
+    //           ''
+    //         )}
+    //       </span>
+    //     </div>
+    //   </div>
+    // );
+
+    const shareContent = <ShareModal />;
 
     const helpModal = <HelpModal />;
 
---
+++ src/components/header/share-modal/index.tsx
@@ -0,0 +1,35 @@
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+import * as EditorActions from '../../../actions/editor';
+import { State } from '../../../constants/default-state';
+import Renderer from './renderer';
+
+export function mapStateToProps(state: State, ownProps) {
+  return {
+    editorString: state.editorString,
+    // lastPosition: state.lastPosition,
+    // manualParse: state.manualParse,
+    mode: state.mode,
+    // vegaLiteSpec: state.vegaLiteSpec,
+    // vegaSpec: state.vegaSpec,
+    view: state.view,
+  };
+}
+
+export function mapDispatchToProps(dispatch: Dispatch<EditorActions.Action>) {
+  return bindActionCreators(
+    {
+      exportVega: EditorActions.exportVega,
+      formatSpec: EditorActions.formatSpec,
+      parseSpec: EditorActions.parseSpec,
+      setScrollPosition: EditorActions.setScrollPosition,
+      toggleAutoParse: EditorActions.toggleAutoParse,
+    },
+    dispatch
+  );
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Renderer);
