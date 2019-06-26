2872639ab9f98caa9e715c9b0df0c71cd8699bf6
siddhant1 <sidhant.manchanda@gmail.com>
2019-06-26 21:00:10 +0530

    Added Prototype switch

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,6 +3,17 @@ import './index.css';
 
 export default class Sidebar extends Component {
   public render() {
-    return <div className="settings">settings</div>;
+    return (
+      <div className="settings">
+        <div className="renderer-switch">
+          <div className="toggleWrapper">
+            <input type="checkbox" id="dn" className="dn" />
+            <label htmlFor="dn" className="toggle">
+              <span className="toggle__handler" />
+            </label>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
