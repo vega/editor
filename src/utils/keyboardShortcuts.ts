9539cd6f28d8570c138f0d09d81b6c0ba41c9b71
Aman Sharma <mannu.poski10@gmail.com>
2019-07-13 01:39:35 +0530

    Add shortcut in help modal for navigating to a symbol

--- src/utils/keyboardShortcuts.ts
+++ src/utils/keyboardShortcuts.ts
@@ -19,6 +19,11 @@ const shortcuts = [
     text: 'Toggle fullscreen mode',
     windows: 'Ctrl + f11',
   },
+  {
+    mac: '⌘ + Shift + o',
+    text: `Navigate to a symbol in the specification whilst focussing on editor`,
+    windows: 'Ctrl + Shift + o',
+  },
 ];
 
 export default shortcuts;
