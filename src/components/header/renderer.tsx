bcda0c3ff506058bf02f109d2a3d68b752c6403e
Aman Sharma <mannu.poski10@gmail.com>
2019-08-18 12:24:00 +0530

    Resolve conflicts with auth-button

--- src/components/header/renderer.tsx
+++ src/components/header/renderer.tsx
@@ -91,8 +91,8 @@ class Header extends React.PureComponent<Props, State> {
             return res.json();
           })
           .then(json => {
-            const { isAuthenticated, name, profilePicUrl } = json;
-            this.props.receiveCurrentUser(isAuthenticated, name, profilePicUrl);
+            const { isAuthenticated, handle, name, profilePicUrl } = json;
+            this.props.receiveCurrentUser(isAuthenticated, handle, name, profilePicUrl);
           })
           .catch(err => {
             // console.error(err);
