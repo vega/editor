2ea76c69dba13f99950ba0f937e10f7da701d90f
chengluyu <chengluyu@live.cn>
2019-09-20 12:59:57 +0800

    Make linter happy

--- App.tsx
+++ App.tsx
@@ -10,17 +10,12 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
