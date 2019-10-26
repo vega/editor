34fee680cbd965cb2a63b248281db63166262ecc
siddhant1 <sidhant.manchanda@gmail.com>
2019-10-26 13:51:53 +0530

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-const { CheckerPlugin } = require("awesome-typescript-loader");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const {CheckerPlugin} = require('awesome-typescript-loader');
 
 module.exports = (env, argv) => {
   const config = {
     entry: {
-      main: "./src/index.tsx"
+      main: './src/index.tsx'
     },
 
     output: {
-      filename: "[name].js",
-      chunkFilename: "[name].js",
-      path: path.resolve(__dirname, "dist"),
+      filename: '[name].js',
+      chunkFilename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
       pathinfo: false
     },
 
     optimization: {
       concatenateModules: false,
       splitChunks: {
-        chunks: "all",
+        chunks: 'all',
         maxInitialRequests: Infinity,
         minSize: 0,
         cacheGroups: {
           vega: {
             test: /vega/,
-            name: "vega",
+            name: 'vega',
             priority: 10,
             reuseExistingChunk: true
           },
           vegaLite: {
             test: /vega-lite/,
-            name: "vega-lite",
+            name: 'vega-lite',
             priority: 20,
             reuseExistingChunk: true
           },
           vendors: {
             test: /[\\/]node_modules[\\/]/,
             priority: -10,
-            name: "vendor",
+            name: 'vendor',
             reuseExistingChunk: true
           },
           default: {
-            name: "default",
+            name: 'default',
             priority: -20,
             reuseExistingChunk: true
           }
@@ -56,11 +56,10 @@ module.exports = (env, argv) => {
       maxAssetSize: 512000
     },
 
-    devtool:
-      argv.mode === "development" ? "cheap-module-source-map" : "source-map",
+    devtool: argv.mode === 'development' ? 'cheap-module-source-map' : 'source-map',
 
     resolve: {
-      extensions: [".ts", ".tsx", ".js", ".mjs", ".json"]
+      extensions: ['.ts', '.tsx', '.js', '.mjs', '.json']
     },
 
     module: {
@@ -68,16 +67,16 @@ module.exports = (env, argv) => {
         {
           test: /\.tsx?$/,
           exclude: /node_modules/,
-          use: ["awesome-typescript-loader"]
+          use: ['awesome-typescript-loader']
         },
         {
           test: /\.css$/,
           use: [
-            "style-loader",
-            "css-loader",
+            'style-loader',
+            'css-loader',
             {
-              loader: "postcss-loader",
-              options: { plugins: [require("autoprefixer")] }
+              loader: 'postcss-loader',
+              options: {plugins: [require('autoprefixer')]}
             }
           ]
         }
@@ -86,17 +85,17 @@ module.exports = (env, argv) => {
 
     plugins: [
       new HtmlWebpackPlugin({
-        filename: "index.html",
-        template: "public/index.html"
+        filename: 'index.html',
+        template: 'public/index.html'
       }),
       new MonacoWebpackPlugin({
-        languages: ["json"]
+        languages: ['json']
       }),
       new CheckerPlugin()
     ],
 
     devServer: {
-      host: "0.0.0.0",
+      host: '0.0.0.0',
       stats: {
         colors: true
       },
@@ -106,9 +105,9 @@ module.exports = (env, argv) => {
         errors: true
       },
       progress: true,
-      stats: "errors-only",
+      stats: 'errors-only',
       open: false,
-      contentBase: path.join(__dirname, "public"),
+      contentBase: path.join(__dirname, 'public'),
       watchContentBase: true,
       watchOptions: {
         ignored: /node_modules/
@@ -116,7 +115,7 @@ module.exports = (env, argv) => {
     },
 
     node: {
-      fs: "empty"
+      fs: 'empty'
     }
   };
 
