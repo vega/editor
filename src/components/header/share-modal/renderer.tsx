b68a25000de910ee4c535567ba0f6e14ccf66b5c
Dominik Moritz <domoritz@gmail.com>
2020-01-27 13:58:04 -0800

    Fix typos in sharing dialog

--- src/components/header/share-modal/renderer.tsx
+++ src/components/header/share-modal/renderer.tsx
@@ -67,8 +67,8 @@ class ShareModal extends React.PureComponent<Props, State> {
     return (
       <>
         <h1>Share</h1>
-        <p>We pack the Vega or Vega-Lite specification and an encoded string in the URL.</p>
-        <p>We use LZ-based compression algorithm and preserve indentation, newlines, and other whitespace.</p>
+        <p>We pack the Vega or Vega-Lite specification as an encoded string in the URL.</p>
+        <p>We use a LZ-based compression algorithm and preserve indentation, newlines, and other whitespace.</p>
         <div>
           <label className="user-pref">
             <input
