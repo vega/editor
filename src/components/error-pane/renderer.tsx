6fcff47e5436393bed95f258438fca8d3723c758
Dominik Moritz <domoritz@gmail.com>
2020-05-28 12:39:11 -0700

    feat: show info logs

--- src/components/error-pane/renderer.tsx
+++ src/components/error-pane/renderer.tsx
@@ -8,6 +8,8 @@ export default class ErrorPane extends React.Component<Props> {
   public render() {
     const list = [];
 
+    let i = 0;
+
     if (this.props.error) {
       list.push(
         <li key={0}>
@@ -16,22 +18,38 @@ export default class ErrorPane extends React.Component<Props> {
         </li>
       );
     }
-    this.props.warns.forEach((warning, i) => {
+
+    this.props.warns.forEach((warning) => {
       list.push(
-        <li key={i + 1}>
+        <li key={i++}>
           <span className="warning">[Warning] </span>
           {warning}
         </li>
       );
     });
+    this.props.infos.forEach((info) => {
+      list.push(
+        <li key={i++}>
+          <span className="info">[Info] </span>
+          {info}
+        </li>
+      );
+    });
+    if (this.props.debugs.length > 0) {
+      <li key={i++}>
+        <span className="debug">[Debug] </span>
+        Debug messages are not shown in the editor. Open the browser console to view debug logs.
+      </li>;
+    }
     if (list.length === 0) {
       list.push(
         <li key={'no error'}>
           <span className="info">[Info] </span>
-          No error or warnings
+          No error, infos, or warnings
         </li>
       );
     }
+
     return (
       <div className="error-pane">
         <ul>{list}</ul>
