803001a6db9af2a4d03f05c31668d1876446c818
chengluyu <chengluyu@live.cn>
2020-09-29 20:59:52 +0800

    fix(EditorPanel): resolve schema request warning

--- index.tsx
+++ index.tsx
@@ -4,5 +4,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./helpers/icon";
 import "./index.pcss";
+import * as monaco from "monaco-editor";
+
+monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+  enableSchemaRequest: true,
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
