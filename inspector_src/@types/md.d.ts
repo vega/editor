b97d0ff957a26980898a10bd85dfe676627566e5
Saul Shanabrook <s.shanabrook@gmail.com>
2021-06-03 18:42:08 -0400

    Remove unused inspector files and move vega2dot to our utils

--- inspector_src/@types/md.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "*.md" {
-  const html: string;
-  export default html;
-}
--- inspector_src/hooks/useResizeObserver.ts
+++
@@ -1,8 +0,0 @@
-import useLazyRef from "./useLazyRef";
-
-export function useResizeObserver(
-  callback: ResizeObserverCallback
-): ResizeObserver {
-  const observerRef = useLazyRef(() => new ResizeObserver(callback));
-  return observerRef.current;
-}
