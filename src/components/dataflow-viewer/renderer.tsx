510cf69313a7919e7de225072617a2e122a44d45
Saul Shanabrook <s.shanabrook@gmail.com>
2021-06-15 17:29:51 -0400

    Add error handling for graph generation

--- src/components/dataflow-viewer/renderer.tsx
+++ src/components/dataflow-viewer/renderer.tsx
@@ -7,7 +7,30 @@ import './index.css';
 
 type StoreProps = ReturnType<typeof mapStateToProps>;
 
-export default function DataflowViewer({runtime}: StoreProps) {
+type State = {
+  error: Error | null;
+};
+
+// Wrap the component so we can catch the errors. We don't use the previously defined
+// error boundary component, since we want to seperate errors in graph generation from
+// errors in spec rendering
+export default class DataflowViewer extends React.Component<StoreProps, State> {
+  state: State = {
+    error: null,
+  };
+  public componentDidCatch(error: Error) {
+    this.setState({error});
+  }
+
+  public render() {
+    if (this.state.error) {
+      return <div id="error-indicator">{this.state.error.message}</div>;
+    }
+    return <DataflowViewerInternal {...this.props} />;
+  }
+}
+
+function DataflowViewerInternal({runtime}: StoreProps) {
   const componentRef = useRef();
 
   const dot = runtime2dot(runtime);
