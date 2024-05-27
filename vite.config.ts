61296c5f882ae8233139d000b307febc06ab511e
Dominik Moritz <domoritz@gmail.com>
2024-05-27 17:35:30 -0400

    try concurrently

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,15 @@
 import react from '@vitejs/plugin-react';
-import childProcess from 'child_process';
+import childProcess from 'node:child_process';
 import type {UserConfig} from 'vite';
 
 const commitHash = childProcess.execSync('git rev-parse --short HEAD').toString();
-
-import type {PluginOption} from 'vite';
-
-function watchNodeModules(modules: string[]): PluginOption {
-  return {
-    name: 'watch-node-modules',
-    config() {
-      return {
-        server: {
-          watch: {
-            ignored: modules.map((m) => `!**/node_modules/${m}/**`),
-          },
-        },
-        optimizeDeps: {
-          exclude: modules,
-        },
-      };
-    },
-  };
-}
 
 const config: UserConfig = {
   base: '/editor/',
   define: {
     __COMMIT_HASH__: JSON.stringify(commitHash),
   },
-  plugins: [
-    react(),
-    watchNodeModules([
-      'vega-lite',
-      'vega',
-      'vega-scale',
-      'vega-embed',
-      'vega-schema-url-parser',
-      'vega-themes',
-      'vega-tooltip',
-    ]),
-  ],
+  plugins: [react()],
   resolve: {
     preserveSymlinks: true,
 
