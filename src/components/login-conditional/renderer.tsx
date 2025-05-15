7c562cd41c487c69d49676bf50a7db8a43272adc
Yuvvan Talreja <31566676+yuvvantalreja@users.noreply.github.com>
2025-05-15 12:10:13 -0400

    fix: Gist methods updated for new auth integration

--- src/components/login-conditional/renderer.tsx
+++ src/components/login-conditional/renderer.tsx
@@ -12,11 +12,13 @@ class LoginConditional extends React.PureComponent<Props> {
 
   handleLogin = (e) => {
     e.preventDefault();
-    // Use a popup window instead of a new tab
-    const popup = window.open(`${BACKEND_URL}auth/github`, 'github-login', 'width=600,height=600');
 
-    // Focus the popup
-    if (popup) popup.focus();
+    const popup = window.open(`${BACKEND_URL}auth/github`, 'github-login', 'width=600,height=600,resizable=yes');
+    if (popup) {
+      popup.focus();
+    } else {
+      window.location.href = `${BACKEND_URL}auth/github`;
+    }
 
     return false;
   };
