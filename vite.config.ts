253d5090a4159026356c286966083084f9b9b38c
Dominik Moritz <domoritz@gmail.com>
2025-11-20 16:49:18 -0500

    fix types

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import {defineConfig} from 'vite';
+import {defineConfig} from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import {resolve} from 'path';
 import {watch as fsWatch, readdirSync, lstatSync, existsSync} from 'fs';
